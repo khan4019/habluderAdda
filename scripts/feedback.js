596d7a643b4c24fc47005854de01651d8973ac40
Khan <khan4019@gmail.com>
2016-01-28 23:22:13 -0600

    style feedback

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,12 +1,25 @@
 var habluGudamRef = new Firebase('https://hablu.firebaseio.com/');
 $('#submitComment').on('click',function (e) {
-  var hablu = $('#name').val() || 'hablu';
-  var email = $('#email').val() || 'habluder@gmail.com';
+  var hablu = $('#name').val();
+  var email = $('#email').val();
   var feedback = $('#feedback').val();
-  var habluDetail = {name:hablu, email:email, feedback:feedback, timestamp: Firebase.ServerValue.TIMESTAMP};
-  habluGudamRef.push(habluDetail);
-  
-  $('#feedback').val('');
+  var rex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if(!feedback){
+    $('#feedback').parent().addClass('has-error');
+  }
+  else if(!email || !rex.test(email)){
+    $('#email').parent().addClass('has-error');
+    $('#feedback').parent().removeClass('has-error');
+  }
+  else if(!hablu){
+    $('#name').parent().addClass('has-error')
+  }
+  else{
+    var habluDetail = {name:hablu, email:email, feedback:feedback, timestamp: Firebase.ServerValue.TIMESTAMP};
+    habluGudamRef.push(habluDetail);
+    $('#feedback').val('');  
+  }
 });
 
 
@@ -23,9 +36,23 @@ function updateList (data) {
 	var docfrag = document.createDocumentFragment();
 
 	for (var i = data.length - 1; i >= 0; i--) {
-		var entry = document.createElement('li');
-		entry.appendChild(document.createTextNode(data[i].feedback));
+		var entry = document.createElement('div');
+    entry.className = 'comment-holder';
+    var nameParagraph = document.createElement('p');
+    
+    var d = (new Date(data[i].timestamp)+'').split(' ');
+    var commentTime = [d[2], d[1], d[3], d[4]].join(' ');
+    nameParagraph.innerHTML = '<span class="comment-name">'+data[i].name+'</span>'+'<span class="comment-time">'+commentTime+'</span>'; 
+    
+    var commentParagraph = document.createElement('p');
+    commentParagraph.innerHTML = '<span class="comment-text">'+ data[i].feedback +'</span>';
+    
+		entry.appendChild(nameParagraph);
+    entry.appendChild(commentParagraph);
 		docfrag.appendChild(entry);
 	};
+
 	$('#latestComments').empty().append(docfrag);  
 }
+
+
