ea3cb18ef63a32469924ae7e5f7afcd36977b8cc
Your Name <AhmmedRasel2@Gmail.com>
2018-02-05 00:30:52 +0600

    fixed - chapter content

---
+++ gosti/js/collpus-button.js
@@ -0,0 +1,43 @@
+
+        $(".ansbtn").click(function () {
+            var lable = $(".ansbtn").text().trim();
+
+            if (lable == "Hide Answer") {
+                $(".ansbtn").text("Show Answer");
+            } else {
+                $(".ansbtn").text("Hide Answer");
+                $(".answer-show").show();
+            }
+        });
+
+        $(".ansbtn2").click(function () {
+            var lable = $(".ansbtn2").text().trim();
+            if (lable == "Hide Answer") {
+                $(".ansbtn2").text("Show Answer");
+            } else {
+                $(".ansbtn2").text("Hide Answer");
+                $(".answer-show2").show();
+            }
+        });
+
+        $(".ansbtn3").click(function () {
+            var lable = $(".ansbtn3").text().trim();
+
+            if (lable == "Hide Answer") {
+                $(".ansbtn3").text("Show Answer");
+            } else {
+                $(".ansbtn3").text("Hide Answer");
+                $(".answer-show3").show();
+            }
+        });
+        $(".ansbtn4").click(function () {
+            var lable = $(".ansbtn4").text().trim();
+
+            if (lable == "Hide Answer") {
+                $(".ansbtn4").text("Show Answer");
+            } else {
+                $(".ansbtn4").text("Hide Answer");
+                $(".answer-show4").show();
+            }
+        });
+
